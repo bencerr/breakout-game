b0a9e7e852071e97949e0274a4a2f0bd90aa79bb
Ben Cerrone <bwc2184@rit.edu>
2022-03-09 23:09:24 -0500

    Score added, more functionality for ball added as well.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById("myCanvas");
+var scoreHeader = document.getElementById("scoreboard")
 var ctx = canvas.getContext("2d")
 
 var x = canvas.width/2;
@@ -6,6 +7,20 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 15;
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+var score = 0;
+
+function displayPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+    ctx.closePath();
+}
 
 function displayBall() {
     ctx.beginPath();
@@ -15,19 +30,51 @@ function displayBall() {
     ctx.closePath();
 }
 
+function mouseHandler(e) {
+    var rect = canvas.getBoundingClientRect();
+    paddleX = (e.screenX - rect.left) - (paddleWidth/2);
+    if (paddleX <= 0) {
+        paddleX = 0;
+    } else if (paddleX + paddleWidth > canvas.width){
+        paddleX = canvas.width - paddleWidth;
+    }
+}
+
+function resetGame() {
+    alert("GAME OVER");
+    document.location.reload();
+    clearInterval(drawInterval);
+    score = 0;
+}
+
+function updateScore() {
+    score++;
+    scoreHeader.innerText = "SCORE: " + score;
+}
+
+document.addEventListener("mousemove", mouseHandler, false);
+
 function draw() {
     ctx.clearRect(0,0,canvas.width, canvas.height);
     displayBall();
+    displayPaddle();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    } else if (y + dy > canvas.height-ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+            updateScore();
+        } else {
+            resetGame()
+        }
     }
 
     x += dx;
     y += dy;
 }
 
-setInterval(draw, 10) // calls draw every 10 seconds
+var drawInterval = setInterval(draw, 10) // calls draw every 10 seconds
